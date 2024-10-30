63f86f17c92f0a417efbfe40d09cebd5c3c5c66e
Dev Bhandari <dev.bhandari@hotmail.com>
2024-10-30 11:50:06 +0530

    Added: currencies endpoint

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
+import requests
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from typing import List
+from functools import lru_cache
 
 from services.db import DB
 from services.scraper import ForexScraper
@@ -29,9 +31,36 @@ class ForexResponse(BaseModel):
     close_rate: float
     volume: int
 
+@lru_cache()
+def fetch_supported_currencies():
+    url = "https://query1.finance.yahoo.com/v1/finance/currencies"
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+    }
+    response = requests.get(url, headers=headers)
+    if response.status_code != 200:
+        raise HTTPException(status_code=500, detail="Failed to fetch currency data")
+    
+    data = response.json()
+    supported_currencies = {currency['shortName']: currency['longName'] for currency in data['currencies']['result']}
+    return supported_currencies
+
+@app.get("/api/supported-currencies")
+async def get_supported_currencies() -> dict:
+    return fetch_supported_currencies()
+
 @app.post("/api/forex-data")
 async def get_forex_data(from_currency: str, to_currency: str, period: str) -> List[ForexResponse]:
-    print(f"Fetching data for {from_currency}-{to_currency} for period {period}...")
+
+    supported_currencies = fetch_supported_currencies()
+    if from_currency not in supported_currencies:
+        raise HTTPException(status_code=400, detail=f"{from_currency} is not a valid currency code.")
+    if to_currency not in supported_currencies:
+        raise HTTPException(status_code=400, detail=f"{to_currency} is not a valid currency code.")
+    
+    if from_currency == to_currency:
+        raise HTTPException(status_code=400, detail="From and To currencies cannot be the same.")
+
     start_date, end_date = db.get_period_dates(period)
     data = db.fetch_from_db(from_currency, to_currency, start_date, end_date)
 
@@ -44,13 +73,10 @@ async def get_forex_data(from_currency: str, to_currency: str, period: str) -> L
                 end_missing.strftime('%Y-%m-%d')
             )
             db.save_to_db(df, from_currency, to_currency)
-        
-        # Re-fetch the complete range to include newly saved data
+
         data = db.fetch_from_db(from_currency, to_currency, start_date, end_date)
     
     return [ForexResponse(**row) for row in data]
-
-
 
 @app.on_event("shutdown")
 def shutdown_event():
